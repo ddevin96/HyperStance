d6d182183f90dfed1131d3780e8ef024c7615f49
Valerio Di Pasquale <valeriodipasq@gmail.com>
2024-03-20 14:59:04 +0100

    First Model

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+from torch_geometric.nn import HypergraphConv
+
+class Model(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super(Model, self).__init__()
+        self.hypergraph_conv_1 = HypergraphConv(in_channels, out_channels)
+    
+    def forward(self, X, edge_index):
+        y = self.hypergraph_conv_1(X, edge_index)
+        y = torch.nn.functional.softmax(y)
+        return y
+    
